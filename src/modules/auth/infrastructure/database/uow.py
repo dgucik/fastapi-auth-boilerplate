07e6ff229ec0b97757212ceda6c4d3926cb0f4de
dgucik <danielgucik7@gmail.com>
2025-12-16 15:39:39 +0100

    Fix mypy

--- src/modules/auth/infrastructure/database/uow.py
+++ src/modules/auth/infrastructure/database/uow.py
@@ -9,10 +9,10 @@ from shared.infrastructure.base_uow import BaseSqlAlchemyUnitOfWork
 class SqlAlchemyUnitOfWork(BaseSqlAlchemyUnitOfWork, AuthUnitOfWork):
     def __init__(self, session_factory: async_sessionmaker[AsyncSession]):
         super().__init__(session_factory)
-        self.accounts: AccountRepository | None = None
+        self.accounts: AccountRepository
 
     async def __aenter__(self) -> "SqlAlchemyUnitOfWork":
         await super().__aenter__()
-        if self._session:
+        if self._session is not None:
             self.accounts = SqlAlchemyAccountRepository(self._session)
         return self
