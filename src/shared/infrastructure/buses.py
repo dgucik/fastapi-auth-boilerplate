59bcf75c5197b8da3a0e27b3dbbca02002d819c0
dgucik <danielgucik7@gmail.com>
2025-12-16 16:50:13 +0100

    Create Buses for specific messages for cqrs

--- src/shared/infrastructure/buses.py
+++ src/shared/infrastructure/buses.py
@@ -1,10 +1,16 @@
-from typing import Generic
-
-from shared.application.cqrs import Handler, MessageBus, TMessage, TResult
+from shared.application.cqrs import (
+    Command,
+    Dto,
+    Handler,
+    MessageBus,
+    Query,
+    TMessage,
+    TResult,
+)
 from shared.infrastructure.exceptions import BusException
 
 
-class SimpleMessageBus(MessageBus[TMessage, TResult], Generic[TMessage, TResult]):
+class GenericMessageBus(MessageBus[TMessage, TResult]):
     def __init__(self, handlers: dict[type[TMessage], Handler[TMessage, TResult]]):
         self._handlers = handlers
 
@@ -13,3 +19,11 @@ class SimpleMessageBus(MessageBus[TMessage, TResult], Generic[TMessage, TResult]
         if not handler:
             raise BusException
         return await handler.handle(command)
+
+
+class CommandBus(GenericMessageBus[Command, None]):
+    pass
+
+
+class QueryBus(GenericMessageBus[Query, Dto]):
+    pass
